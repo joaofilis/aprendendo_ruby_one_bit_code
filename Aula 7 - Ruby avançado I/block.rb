26a2becbc30a3e473298bc723ae08d8ad353f58b
joao.filismino <joao.filismino@inmetrics.com.br>
2019-07-29 17:16:37 -0300

    aula 8, 9 e 10 ainda terão ajustes

---
+++ Aula 7 - Ruby avançado I/block.rb
@@ -0,0 +1,14 @@
+# 5.times {puts 'Exec the block'}
+
+# sum = 0
+# numbers = [5, 10, 5]
+# numbers.each {|number| sum += number }
+# puts sum
+
+# def foo
+#     #call the block 
+#     yield
+#     yield
+# end
+
+# foo {puts 'Exec the block'}
